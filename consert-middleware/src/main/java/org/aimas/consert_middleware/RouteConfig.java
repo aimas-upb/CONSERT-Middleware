2f4076837ef8ca9c4fedddae93ba28449abdbdf0
InternationalKoder <international_koder@yahoo.com>
2017-06-14 09:47:30 +0300

    Add router configuration structure

---
+++ consert-middleware/src/main/java/org/aimas/consert_middleware/RouteConfig.java
@@ -0,0 +1,62 @@
+package org.aimas.consert_middleware;
+
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.Router;
+
+/**
+ * Defines the configuration of a router
+ */
+public abstract class RouteConfig {
+
+	public static final String API_ROUTE = "/api";
+	public static final String SENSING_ROUTE = "/sensing";
+	public static final String USAGE_ROUTE = "/usage";
+	public static final String COORDINATION_ROUTE = "/coordination";
+	public static final String DISSEMINATION_ROUTE = "/dissemination";
+	public static final String MANAGEMENT_ROUTE = "/management";
+	
+	
+	/**
+	 * Creates a router configured to be used on a CtxSensor agent
+	 * @param vertx the Vertx instance
+	 * @return the created router
+	 */
+	public abstract Router createRouterSensing(Vertx vertx);
+	
+	/**
+	 * Creates a router configured to be used on a CtxUseragent
+	 * @param vertx the Vertx instance
+	 * @return the created router
+	 */
+	public abstract Router createRouterUsage(Vertx vertx);
+	
+	/**
+	 * Creates a router configured to be used on a CtxCoordagent
+	 * @param vertx the Vertx instance
+	 * @return the created router
+	 */
+	public abstract Router createRouterCoordination(Vertx vertx);
+	
+	/**
+	 * Creates a router configured to be used on a CtxQueryHandleragent
+	 * @param vertx the Vertx instance
+	 * @return the created router
+	 */
+	public abstract Router createRouterDissemination(Vertx vertx);
+	
+	/**
+	 * Creates a router configured to be used on an OrgMgr agent
+	 * @param vertx the Vertx instance
+	 * @return the created router
+	 */
+	public abstract Router createRouterManagement(Vertx vertx);
+	
+	/**
+	 * Creates a new router with no route
+	 * @param vertx the Vertx instance
+	 * @return the created router
+	 */
+	protected Router createRouter(Vertx vertx) {
+		return Router.router(vertx);
+	}
+}
---
+++ consert-middleware/src/main/java/org/aimas/consert_middleware/RouteConfigV1.java
@@ -0,0 +1,82 @@
+package org.aimas.consert_middleware;
+
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.Router;
+
+public class RouteConfigV1 extends RouteConfig {
+
+	@Override
+	public Router createRouterSensing(Vertx vertx) {
+		
+		Router router = this.createRouter(vertx);
+		
+		router.put(RouteConfig.API_ROUTE + RouteConfig.SENSING_ROUTE + "/tasking_command/").handler(null);
+		
+		return router;
+	}
+
+	@Override
+	public Router createRouterUsage(Vertx vertx) {
+		
+		Router router = this.createRouter(vertx);
+		
+		router.put(RouteConfig.API_ROUTE + RouteConfig.USAGE_ROUTE + "/tasking_command/").handler(null);
+		
+		return router;
+	}
+
+	@Override
+	public Router createRouterCoordination(Vertx vertx) {
+		
+		Router router = this.createRouter(vertx);
+		
+		router.post(RouteConfig.API_ROUTE + RouteConfig.COORDINATION_ROUTE + "/context_assertions/").handler(null);
+		router.get(RouteConfig.API_ROUTE + RouteConfig.COORDINATION_ROUTE + "/context_assertions/").handler(null);
+		router.get(RouteConfig.API_ROUTE + RouteConfig.COORDINATION_ROUTE + "/context_assertions/:id/").handler(null);
+		router.put(RouteConfig.API_ROUTE + RouteConfig.COORDINATION_ROUTE + "/context_assertions/:id/").handler(null);
+		router.delete(RouteConfig.API_ROUTE + RouteConfig.COORDINATION_ROUTE + "/context_assertions/:id/").handler(null);
+		
+		router.post(RouteConfig.API_ROUTE + RouteConfig.COORDINATION_ROUTE + "/assertion_capability_subscriptions/").handler(null);
+		router.get(RouteConfig.API_ROUTE + RouteConfig.COORDINATION_ROUTE + "/assertion_capability_subscriptions/:id/").handler(null);
+		router.put(RouteConfig.API_ROUTE + RouteConfig.COORDINATION_ROUTE + "/assertion_capability_subscriptions/:id/").handler(null);
+		router.delete(RouteConfig.API_ROUTE + RouteConfig.COORDINATION_ROUTE + "/assertion_capability_subscriptions/:id/").handler(null);
+		
+		router.post(RouteConfig.API_ROUTE + RouteConfig.COORDINATION_ROUTE + "/insert_context_assertion/").handler(null);
+		router.post(RouteConfig.API_ROUTE + RouteConfig.COORDINATION_ROUTE + "/insert_entity_descriptions/").handler(null);
+		router.post(RouteConfig.API_ROUTE + RouteConfig.COORDINATION_ROUTE + "/update_entity_descriptions/").handler(null);
+		
+		router.post(RouteConfig.API_ROUTE + RouteConfig.COORDINATION_ROUTE + "/activate_context_assertion/").handler(null);
+		router.post(RouteConfig.API_ROUTE + RouteConfig.COORDINATION_ROUTE + "/register_query_handler/").handler(null);
+		router.post(RouteConfig.API_ROUTE + RouteConfig.COORDINATION_ROUTE + "/unregister_query_handler/").handler(null);
+		
+		return router;
+	}
+
+	@Override
+	public Router createRouterDissemination(Vertx vertx) {
+		
+		Router router = this.createRouter(vertx);
+		
+		router.post(RouteConfig.API_ROUTE + RouteConfig.DISSEMINATION_ROUTE + "/unregister_query_user/").handler(null);
+		router.get(RouteConfig.API_ROUTE + RouteConfig.DISSEMINATION_ROUTE + "/context_query/").handler(null);
+		router.post(RouteConfig.API_ROUTE + RouteConfig.DISSEMINATION_ROUTE + "/context_subscriptions/").handler(null);
+		router.get(RouteConfig.API_ROUTE + RouteConfig.DISSEMINATION_ROUTE + "/context_subscriptions/:id/").handler(null);
+		router.put(RouteConfig.API_ROUTE + RouteConfig.DISSEMINATION_ROUTE + "/context_subscriptions/:id/").handler(null);
+		router.delete(RouteConfig.API_ROUTE + RouteConfig.DISSEMINATION_ROUTE + "/context_subscriptions/:id/").handler(null);
+		
+		return router;
+	}
+
+	@Override
+	public Router createRouterManagement(Vertx vertx) {
+		
+		Router router = this.createRouter(vertx);
+		
+		router.post(RouteConfig.API_ROUTE + RouteConfig.MANAGEMENT_ROUTE + "/context_agents/").handler(null);
+		router.get(RouteConfig.API_ROUTE + RouteConfig.MANAGEMENT_ROUTE + "/find_coordinator/").handler(null);
+		router.get(RouteConfig.API_ROUTE + RouteConfig.MANAGEMENT_ROUTE + "/find_query_handler/").handler(null);
+		
+		return router;
+	}
+
+}
